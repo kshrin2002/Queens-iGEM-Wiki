152531f59e3941b14893ef58493496bcb57a749c
Mariam El-Behiry <19meb17@queensu.ca>
2024-06-20 23:26:12 +0000

    Update description.tsx

--- src/contents/description.tsx
+++ src/contents/description.tsx
@@ -45,27 +45,26 @@ export function Description() {
 
       <div className="row mt-4">
         <div className="col-lg-8">
-          <h2>Some advice</h2>
+          <h2>Parkinson's Disease (PD)</h2>
           <hr />
-          <p>
-            We encourage you to put up a lot of information and content on your
-            wiki, but we also encourage you to include summaries as much as
-            possible. If you think of the sections in your project description
-            as the sections in a publication, you should try to be concise,
-            accurate, and unambiguous in your achievements. Your Project
-            Description should include more information than your project
-            abstract.
-          </p>
-        </div>
-        <div className="col-lg-4">
-          <h2>References</h2>
-          <hr />
-          <p>
-            iGEM teams are encouraged to record references you use during the
-            course of your research. They should be posted somewhere on your
-            wiki so that judges and other visitors can see how you thought about
-            your project and what works inspired you.
-          </p>
+          <div>
+            <h3><em>Introduction</em></h3>
+            <p>
+             Parkinson’s disease (PD) is a chronic and progressive neurodegenerative condition characterized by the degeneration of dopaminergic neurons in the brain. This disruption leads to a spectrum of motor symptoms including rigidity, bradykinesia, difficulty walking, and resting tremors. However, PD is not solely a movement disorder; it also encompasses a wide range of non-motor symptoms, including cognitive impairment, gastrointestinal dysfunction, and psychiatric disturbances. (Kouli et al., 2018)
+            </p>
+          </div>
+          <div>
+            <h3><em>Global Impact and Prevalence</em></h3>
+            <p>
+              According to the World Health Organization, the prevalence of PD has doubled in the past 25 years. Global estimates in 2019 showed over 8.5 million individuals with PD. As the global population ages, the prevalence of PD is expected to rise, further amplifying the socio-economic impact of the disease. It is estimated that the number of individuals with PD will double by 2040, with the greatest increases projected to occur in low- and middle-income countries. (World Health Organization, 2023)
+           </p>
+          </div>
+          <div>
+            <h3><em>Current Treatments and Limitations</em></h3>
+            <p>
+              Despite significant advancements in PD research and drug development, current therapies primarily focus on symptom management and fail to address disease progression. Levodopa, the gold standard treatment for PD, provides temporary symptomatic relief but does not address the underlying neurodegenerative process, while exacerbating non-motor symptoms. (Murakami et al., 2023) Therefore, there is an urgent need for a clinically feasible therapy that target the underlying pathophysiological mechanisms of PD. 
+            </p>
+          </div>
         </div>
       </div>
     </>
