a68acbf179b3c764a6031531b398b58ab9e70e3e
Mariam El-Behiry <19meb17@queensu.ca>
2024-06-20 21:18:02 +0000

    Update description.tsx

--- src/contents/description.tsx
+++ src/contents/description.tsx
@@ -40,11 +40,18 @@ export function Description() {
             </p>
           </div>
           <div>
-            <h3><em>Prevalence and Global Impact</em></h3>
+            <h3><em>Global Impact and Prevalence</em></h3>
             <p>
-              According to the World Health Organization, the prevalence of PD has doubled in the past 25 years. Global estimates in 2019 showed over 8.5 million individuals with PD. As the global population ages, the prevalence of PD is expected to rise, further amplifying the socio-economic impact of the disease. It is estimated that the number of individuals with PD will double by 2040, with the greatest increases projected to occur in low- and middle-income countries. 
+              According to the World Health Organization, the prevalence of PD has doubled in the past 25 years. Global estimates in 2019 showed over 8.5 million individuals with PD. As the global population ages, the prevalence of PD is expected to rise, further amplifying the socio-economic impact of the disease. It is estimated that the number of individuals with PD will double by 2040, with the greatest increases projected to occur in low- and middle-income countries. (World Health Organization, 2023)
             </p>
           </div>
+          <div>
+           <h3><em>Current Treatments and Limitations</em></h3>
+            <p>
+              Despite significant advancements in PD research and drug development, current therapies primarily focus on symptom management and fail to address disease progression. Levodopa, the gold standard treatment for PD, provides temporary symptomatic relief but does not address the underlying neurodegenerative process, while exacerbating non-motor symptoms. (Murakami et al., 2023) Therefore, there is an urgent need for a clinically feasible therapy that target the underlying pathophysiological mechanisms of PD. 
+              </p>
+          </div>
+          <div>
         </div>
         <Inspirations inspirationLinkList={links} />
       </div>
