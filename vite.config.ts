91c0c473546d988b4b38c263734397c12bc8d4da
Tigh <20tpg1@queensu.ca>
2024-08-29 22:43:06 +0000

    Replace vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { stringToSlug } from "./src/utils";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/queens-canada/',
-  plugins: [react()],
-  build: {
-    outDir: 'public', // Specify the output directory as 'public'
-  },
-});
-
+export default () => {
+  const env = loadEnv("dev", process.cwd());
+  return defineConfig({
+    base: `/${stringToSlug(env.VITE_TEAM_NAME)}/`,
+    plugins: [react()]
+  });
+};
