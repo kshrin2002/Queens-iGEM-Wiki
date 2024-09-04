f846b57aecf92b730da9c065d31dc05f2f0c94d0
Ali Cotten <20aec6@queensu.ca>
2024-09-04 21:15:04 +0000

    ali- test

--- src/contents/contribution.tsx
+++ src/contents/contribution.tsx
@@ -5,23 +5,31 @@ export function Contribution() {
         <div class="intro">
             <h1>Subtitle 1</h1>
             <h2>Section 1</h2>
-		        <h2>Section 2</h2>		
+            <p>put something here</p>
+		        <h2>Section 2</h2>	
+            <p>put something here</p>	
         </div>
       </div>
     <div class="section" id="subtitle2">
       <h1>Subtitle 2</h1>
       <h2>Section 1</h2>
+      <p>put something here</p>
       <h2>Section 2</h2>
+      <p>put something here</p>
     </div>
     <div class="section" id="subtitle3">
       <h1>Subtitle 3</h1>
       <h2>Section 1</h2>
+      <p>put something here</p>
       <h2>Section 2</h2>
+      <p>put something here</p>
     </div>
     <div class="section" id="subtitle4">
       <h1>Subtitle 4</h1>
       <h2>Section 1</h2>
+      <p>put something here</p>
       <h2>Section 2</h2>
+      <p>put something here</p>
     </div>
 </div>
 );
