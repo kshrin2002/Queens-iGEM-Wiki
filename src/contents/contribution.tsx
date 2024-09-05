e54f252060d093312733567bd71bc5c298fd3570
Ali Cotten <20aec6@queensu.ca>
2024-09-05 22:49:37 +0000

    ali - testing contribution work

--- src/contents/contribution.tsx
+++ src/contents/contribution.tsx
@@ -1,34 +1,25 @@
 export function Contribution() {
   return (
     <>
-      <div className="row mt-4">
-        <div className="col">
-          <div className="bd-callout bd-callout-info">
-            <h4>Bronze Medal Criterion #4</h4>
-            <p>
-              Make a useful contribution for future iGEM teams. Use this page to
-              document that contribution.
-            </p>
-            <p>
-              If you are making a contribution by adding information to an
-              existing Part or creating a new Part, you must document your
-              contribution on the Part's Main Page on the{" "}
-              <a href="https://parts.igem.org/Main_Page">Registry</a> for your
-              team to be eligible for this criteria. You can use this page to
-              link to that part and include additional information about your
-              contribution.
-            </p>
-            <hr />
-            <p>
-              Please see the{" "}
-              <a href="https://competition.igem.org/judging/medals">
-                2024 Medals Page
-              </a>{" "}
-              for more information.
-            </p>
-          </div>
-        </div>
-      </div>
+        <TitleSection title={'hello'} subtitle1={''}/>
+        <TitleSection />
+        <TitleSection />
+        <TitleSection />
     </>
   );
 }
+
+const TitleSection = (title, subtitle1, description1, subtitle2, description2, image) => <>
+    <h1>{title}</h1>
+    <div className='d-flex'>
+        <div>
+            <h3>{subtitle1}</h3>
+            <span>{description1}</span>
+            <h3>{subtitle2}</h3>
+            <span>{description2}</span>
+        </div>
+        <div>
+            <img src={image} />
+        </div>
+    </div>
+</>
