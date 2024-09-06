87ae4a012e42e8d3b3d3d87dccc01c77bbf47a1a
Ali Cotten <20aec6@queensu.ca>
2024-09-06 18:52:01 +0000

    ali - contributions page

--- src/contents/contribution.tsx
+++ src/contents/contribution.tsx
@@ -1,35 +1,32 @@
 export function Contribution() {
-    return (
-      <>
-        <div className="row mt-4">
-          <div className="col">
-            <div className="bd-callout bd-callout-info">
-              <h4>Bronze Medal Criterion #4</h4>
-              <p>
-                Make a useful contribution for future iGEM teams. Use this page to
-                document that contribution.
-              </p>
-              <p>
-                If you are making a contribution by adding information to an
-                existing Part or creating a new Part, you must document your
-                contribution on the Part's Main Page on the{" "}
-                <a href="https://parts.igem.org/Main_Page">Registry</a> for your
-                team to be eligible for this criteria. You can use this page to
-                link to that part and include additional information about your
-                contribution.
-              </p>
-              <hr />
-              <p>
-                Please see the{" "}
-                <a href="https://competition.igem.org/judging/medals">
-                  2024 Medals Page
-                </a>{" "}
-                for more information.
-              </p>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <TitleSection 
+        title={'SUBTITLE 1'} 
+        subtitle1={'Section 1'} 
+        description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'} 
+        subtitle2={'Section 2'} 
+        description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'} 
+        image={'/path-to-image'} 
+        borderColor={'blue'}
+      />
+    </>
+  );
+}
+
+const TitleSection: React.FC<TitleSectionProps> = ({title, subtitle1, description1, subtitle2, description2, image, borderColor }) => (
+  <div className="card" style={{ borderColor: borderColor }}>
+    <h1 className="title">{title}</h1>
+    <div className="content">
+      <div className="text-content">
+        <h3>{subtitle1}</h3>
+        <p>{description1}</p>
+        <h3>{subtitle2}</h3>
+        <p>{description2}</p>
+      </div>
+      <div className="image-content">
+        <img src="https://static.igem.wiki/teams/5079/headshot-template-picture.png" alt="section visual" />
+      </div>
+    </div>
+  </div>
+);
