68742340c25cd184f8966e53a9d0cea840124b3e
Ali Cotten <20aec6@queensu.ca>
2024-09-06 19:11:32 +0000

    ali - adding another subtitle

--- src/contents/contribution.tsx
+++ src/contents/contribution.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function Contribution() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <>
       <TitleSection 
         title={'SUBTITLE 1'} 
         subtitle1={'Section 1'} 
@@ -12,7 +12,16 @@ export function Contribution() {
         image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
         borderColor={'blue'}
       />
-    </div>
+       <TitleSection 
+        title={'SUBTITLE 2'} 
+        subtitle1={'Section 1'} 
+        description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
+        subtitle2={'Section 2'} 
+        description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
+        image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
+        borderColor={'blue'}
+      />
+    </>
   );
 }
 
@@ -26,11 +35,11 @@ interface TitleSectionProps {
   borderColor: string;
 }
 
-const TitleSection: React.FC<TitleSectionProps> = ({ title, subtitle1, description1, subtitle2, description2, image, borderColor }) => (
+const TitleSection: React.FC<TitleSectionProps> = ({title, subtitle1, description1, subtitle2, description2, image, borderColor }) => (
   <div className="card" style={{ borderColor: borderColor, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-    <div className="text-content" style={{ flex: 1, paddingRight: '20px', position: 'relative' }}>
+    <div className="text-content" style={{ flex: 1, paddingRight: '20px' }}>
       <h1 className="title">{title}</h1>
-      <h3 style={{ textAlign: 'center', margin: '0', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>{subtitle1}</h3>
+      <h3>{subtitle1}</h3>
       <p>{description1}</p>
       <h3>{subtitle2}</h3>
       <p>{description2}</p>
