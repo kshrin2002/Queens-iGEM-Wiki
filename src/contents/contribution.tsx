969c47ac1e5cd9a07baa15a5f8eca8e13e0b9bbd
Ali Cotten <20aec6@queensu.ca>
2024-09-06 19:01:08 +0000

    ali - adding flex box

--- src/contents/contribution.tsx
+++ src/contents/contribution.tsx
@@ -9,7 +9,7 @@ export function Contribution() {
         description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'} 
         subtitle2={'Section 2'} 
         description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'} 
-        image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'}
+        image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
         borderColor={'blue'}
       />
     </>
@@ -27,18 +27,16 @@ interface TitleSectionProps {
 }
 
 const TitleSection: React.FC<TitleSectionProps> = ({title, subtitle1, description1, subtitle2, description2, image, borderColor }) => (
-  <div className="card" style={{ borderColor: borderColor }}>
-    <h1 className="title">{title}</h1>
-    <div className="content">
-      <div className="text-content">
-        <h3>{subtitle1}</h3>
-        <p>{description1}</p>
-        <h3>{subtitle2}</h3>
-        <p>{description2}</p>
-      </div>
-      <div className="image-content">
-        <img src={image} alt="section visual" />
-      </div>
+  <div className="card" style={{ borderColor: borderColor, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <div className="text-content" style={{ flex: 1, paddingRight: '20px' }}>
+      <h1 className="title">{title}</h1>
+      <h3>{subtitle1}</h3>
+      <p>{description1}</p>
+      <h3>{subtitle2}</h3>
+      <p>{description2}</p>
+    </div>
+    <div className="image-content" style={{ flex: 0 }}>
+      <img src={image} alt="section visual" style={{ width: '250px', height: '250px', borderRadius: '50%' }} />
     </div>
   </div>
 );
