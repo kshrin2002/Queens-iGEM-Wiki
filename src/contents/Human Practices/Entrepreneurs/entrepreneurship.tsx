6af8d9e8a71015050a6638d0208302c1c1c76948
Ali Cotten <20aec6@queensu.ca>
2024-09-19 22:50:46 +0000

    ali - add template to entrepreneurship

--- src/contents/Human Practices/Entrepreneurs/entrepreneurship.tsx
+++ src/contents/Human Practices/Entrepreneurs/entrepreneurship.tsx
@@ -1,5 +1,38 @@
-//import { Inspirations, InspirationLink } from "../components";
+//import React from 'react';
 
 export function Entrepreneurship() {
-    return null;
-  }
+  return (
+    <>
+      <EntrepreneurshipHeading />
+
+      <OverviewSection />
+
+    </>
+  );
+}
+
+const OverviewSection = () => {
+  return (
+    <section style={{ backgroundColor: '#FE9BA1', color: 'white', padding: '20px', marginTop: '100px', width: '1520px', height: '350px', marginBottom: '100px' }}>
+      <h2>Overview</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
+  );
+};
+
+const EntrepreneurshipHeading: React.FC = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '138px' }}>
+      <h1 style={{ fontSize: '5em', color: '#590000', fontWeight: 'bold', letterSpacing: '2px' }}>
+        ENTREPRENEURSHIP
+      </h1>
+    </div>
+  );
+};
+
+export default Entrepreneurship;
