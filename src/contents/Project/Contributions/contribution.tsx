1f2f8546df6efcbd6fd285f4f0312bd51b715dfc
Ali Cotten <20aec6@queensu.ca>
2024-09-19 02:08:25 +0000

    ali - add space between first card and overview

--- src/contents/Project/Contributions/contribution.tsx
+++ src/contents/Project/Contributions/contribution.tsx
@@ -17,6 +17,7 @@ export function Contribution() {
         description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
         image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
         borderColor={'#E55157'}
+        style={{ marginTop: '100px' }}
       />
       <TitleSection 
         title={'SUBTITLE 2'} 
