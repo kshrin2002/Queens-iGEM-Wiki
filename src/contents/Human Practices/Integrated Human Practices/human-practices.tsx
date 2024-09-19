ac19e20bcc36c1802bb7ca2beb3bb5a38388baf9
Ali Cotten <20aec6@queensu.ca>
2024-09-19 22:53:10 +0000

    ali - add template to IHP

--- src/contents/Human Practices/Integrated Human Practices/human-practices.tsx
+++ src/contents/Human Practices/Integrated Human Practices/human-practices.tsx
@@ -12,43 +12,22 @@ export function HumanPractices() {
 
 const OverviewSection = () => {
   return (
-    <section
-      style={{ backgroundColor: "maroon", color: "white", padding: "20px" }}
-    >
+    <section style={{ backgroundColor: '#FE9BA1', color: 'white', padding: '20px', marginTop: '100px', width: '1520px', height: '350px', marginBottom: '100px' }}>
       <h2>Overview</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </section>
   );
 };
 
-const IHPHeading = () => {
+const IHPHeading: React.FC = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "30px" }}>
-      <h1
-        style={{
-          fontSize: "5em",
-          color: "#590000",
-          fontWeight: "bold",
-          letterSpacing: "2px",
-        }}
-      >
+    <div style={{ textAlign: 'center', marginTop: '138px' }}>
+      <h1 style={{ fontSize: '5em', color: '#590000', fontWeight: 'bold', letterSpacing: '2px' }}>
         INTEGRATED HUMAN PRACTICES
       </h1>
     </div>
