e9d69ac1d625bf6c58e8158e657bb3e70d0043e0
Ali Cotten <20aec6@queensu.ca>
2024-09-20 19:41:24 +0000

    ali - make podcast section

--- src/contents/Human Practices/Education/education.tsx
+++ src/contents/Human Practices/Education/education.tsx
@@ -24,6 +24,7 @@ export function Education() {
       // researchers heading
       <ResearchersHeading />
 
+      <PodcastSection />
     </>
   );
 }
@@ -126,6 +127,17 @@ const ResearchersHeading: React.FC = () => {
       </h2>
       <div style={{ borderTop: '2px solid #590000', width: '350px' }}></div>
     </div>
+  );
+};
+
+const PodcastSection = () => {
+  return (
+    <section style={{ backgroundColor: '#FE9BA1', color: 'white', padding: '20px', marginTop: '100px', width: '1140px', height: '250px', marginBottom: '100px' }}>
+      <h2>Podcast</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </section>
   );
 };
 
