48315c6b28fd57769c69b80b15c7443f1b541faf
Ali Cotten <20aec6@queensu.ca>
2024-09-21 23:15:35 +0000

    ali - fix positioning

--- src/contents/Project/Contributions/contribution.tsx
+++ src/contents/Project/Contributions/contribution.tsx
@@ -34,7 +34,6 @@ const OverviewSection = () => {
     </section>
   );
 };
-
 const CardSection: React.FC = () => {
   const sections = [
     {
@@ -76,15 +75,13 @@ const CardSection: React.FC = () => {
             <Card.Body>
               <Card.Title className="center-title">{section.title}</Card.Title>
               <Row>
-                <Col xs={12} className="text-container">
+                <Col xs={8} className="text-container">
                   <h3>{section.subtitle1}</h3>
                   <Card.Text>{section.description1}</Card.Text>
                   <h3>{section.subtitle2}</h3>
                   <Card.Text>{section.description2}</Card.Text>
                 </Col>
-              </Row>
-              <Row>
-                <Col xs={12}>
+                <Col xs={4}>
                   <img src={placeholderImage} alt={`Image for ${section.title}`} style={{ width: '100%', height: 'auto' }} />
                 </Col>
               </Row>
