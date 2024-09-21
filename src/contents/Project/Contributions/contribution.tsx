9fe9c7b3fe1ed0c1ee1675000a8f452c88be676a
Ali Cotten <20aec6@queensu.ca>
2024-09-21 22:55:14 +0000

    ali - import wet lab cards to project

--- src/contents/Project/Contributions/contribution.tsx
+++ src/contents/Project/Contributions/contribution.tsx
@@ -10,46 +10,10 @@ export function Contribution() {
 
       <OverviewSection />
 
-      <div className="title-sections-container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', marginRight: '50px' }}>
-        <TitleSection 
-          title={'SUBTITLE 1'} 
-          subtitle1={'Section 1'} 
-          description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          subtitle2={'Section 2'} 
-          description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
-          borderColor={'#E55157'}
-        />
-        <TitleSection 
-          title={'SUBTITLE 2'} 
-          subtitle1={'Section 1'} 
-          description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          subtitle2={'Section 2'} 
-          description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
-          borderColor={'#E55157'}
-        />
-        <TitleSection 
-          title={'SUBTITLE 3'} 
-          subtitle1={'Section 1'} 
-          description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          subtitle2={'Section 2'} 
-          description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
-          borderColor={'#E55157'}
-        />
-        <TitleSection 
-          title={'SUBTITLE 4'} 
-          subtitle1={'Section 1'} 
-          description1={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          subtitle2={'Section 2'} 
-          description2={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'} 
-          image={'https://static.igem.wiki/teams/5079/headshot-template-picture.png'} 
-          borderColor={'#E55157'}
-        />
+      <div className="main-content">
+        <Sidebar />
+        <CardSection />
       </div>
-      
-      <Sidebar />
     
       <BackToTopButton />
     </>
@@ -80,28 +44,34 @@ const OverviewSection = () => {
   );
 };
 
-const TitleSection: React.FC<TitleSectionProps> = ({title, subtitle1, description1, subtitle2, description2, image, borderColor }) => (
-  <div className="card" style={{ 
-      border: `5px solid ${borderColor}`, 
-      borderRadius: '20px', 
-      display: 'flex', 
-      flexDirection: 'row', 
-      alignItems: 'flex-start', 
-      padding: '20px', 
-      marginBottom: '100px' 
-    }}>
-    <div className="text-content" style={{ textAlign: 'left', maxWidth: '600px' }}>
-      <h1 className="title" style={{ textAlign: 'center' }}>{title}</h1>
-      <h3 style={{ fontFamily: "'Open Sans Hebrew', sans-serif" }}>{subtitle1}</h3>
-      <p style={{ fontFamily: "'Open Sans Hebrew', sans-serif" }}>{description1}</p>
-      <h3 style={{ fontFamily: "'Open Sans Hebrew', sans-serif" }}>{subtitle2}</h3>
-      <p style={{ fontFamily: "'Open Sans Hebrew', sans-serif" }}>{description2}</p>
-    </div>
-    <div className="image-content" style={{ marginTop: '20px' }}>
-      <img src={image} alt="section visual" style={{ width: '250px', height: '150px' }} />
-    </div>
-  </div>
-);
+const CardSection: React.FC = () => {
+  const sections = ['Subtitle 1', 'Subtitle 2', 'Subtitle 3', 'Subtitle 4'];
+  const placeholderImage = "https://via.placeholder.com/150"; // Placeholder image for now
+
+  return (
+    <Container fluid className="card-container">
+      {sections.map((section, index) => (
+        <div key={index} id={`section-${index}`} className="card-wrapper">
+          <Card className="custom-card">
+            <Card.Body>
+              <Card.Title className="center-title">{section}</Card.Title>
+              <Row>
+                <Col xs={8} className="text-container">
+                  <Card.Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vel mauris eros.
+                  </Card.Text>
+                </Col>
+                <Col xs={4}>
+                  <img src={placeholderImage} alt={`Image for ${section}`} style={{ width: '100%', height: 'auto' }} />
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </Container>
+  );
+};
 
 const ContributionHeading: React.FC = () => {
   return (
