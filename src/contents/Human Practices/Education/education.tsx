2ce7d7b3fd7c67ca478de30e1f93f65c488eb29d
Ali Cotten <20aec6@queensu.ca>
2024-09-25 00:44:33 +0000

    add a social media section

--- src/contents/Human Practices/Education/education.tsx
+++ src/contents/Human Practices/Education/education.tsx
@@ -17,6 +17,7 @@ export function Education() {
       <CommunityHeading />
       <PSWSection />
       <CommunityCarousel1 />
+      <SocialMediaSection />
     </>
   );
 }
@@ -364,5 +365,17 @@ const CommunityCarousel1 = () => {
   );
 };
 
+const SocialMediaSection = () => {
+  return (
+    <div className="section-container">
+      <section className="social-media-section">
+        <h2>Workshop</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+    </div>
+  );
+};
 
 export default Education;
