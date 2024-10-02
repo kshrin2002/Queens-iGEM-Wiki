83e77f5ab212c17e3a12e9f549ce23d3ac6e113e
Shrinidhi <21stsk@queensu.ca>
2024-10-02 05:11:50 +0000

    taking out fiedit bc apparently it was never used

--- src/contents/Wet Lab/Safety/safety.tsx
+++ src/contents/Wet Lab/Safety/safety.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, Row, Col, Container, Button } from 'react-bootstrap';
-import { FiEdit } from "react-icons/fi";
 import { motion } from "framer-motion";
 import { useEffect, useState } from 'react';
 
